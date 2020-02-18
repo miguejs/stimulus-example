61bfba0c941f665b1ed8700ec8dd9b6ea507a78c
miguejs <imiked.mail@gmail.com>
2020-02-18 16:59:33 -0600

    Adding  targets

--- app/javascript/packs/controllers/portrait_controller.js
+++ app/javascript/packs/controllers/portrait_controller.js
@@ -1,7 +1,9 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
+  static targets = [ "image"]
   connect() {
     console.log("Hello, Stimulus!", this.element)
-  }
+    console.log("Portrait image is ", this.imageTarget)
+  } 
 }
