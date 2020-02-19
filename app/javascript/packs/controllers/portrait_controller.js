617ecfe7761eefac2531787084c72ede3623ec20
miguejs <imiked.mail@gmail.com>
2020-02-19 14:46:41 -0600

    Adding actions

--- app/javascript/packs/controllers/portrait_controller.js
+++ app/javascript/packs/controllers/portrait_controller.js
@@ -5,5 +5,20 @@ export default class extends Controller {
   connect() {
     console.log("Hello, Stimulus!", this.element)
     console.log("Portrait image is ", this.imageTarget)
-  } 
+  }
+
+  changePicture(event) {
+    console.log("Fired Event", event)
+    console.log("Element with the data action", event.currentTarget)
+    button = event.currentTarget
+
+    button.disabled = true
+    this.imageTarget.classList.add('disabled')
+
+    fetch('https://picsum.photos/700/500').then((response) => {
+      this.imageTarget.classList.remove('disabled')
+      button.disabled = false
+      this.imageTarget.src = response.url
+    })
+  }
 }
