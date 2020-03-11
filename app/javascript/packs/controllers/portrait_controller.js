3bcf91d7d7edcd65500a5f930131e15725b29f5d
miguejs <imiked.mail@gmail.com>
2020-03-10 18:40:20 -0600

    upload fixes to scene

--- app/javascript/packs/controllers/portrait_controller.js
+++ app/javascript/packs/controllers/portrait_controller.js
@@ -2,22 +2,19 @@ import { Controller } from 'stimulus'
 
 export default class extends Controller {
   static targets = ['image']
+
   connect() {
-    console.log("Hello, Stimulus!", this.element)
-    console.log("Portrait image is ", this.imageTarget)
+    console.log("stimulus is working")
+    console.log("image target is ", this.imageTarget)
   }
 
   changePicture(event) {
-    console.log("Fired Event", event)
-    console.log("Element with the data action", event.currentTarget)
-    var button = event.currentTarget
-
+    let button = event.currentTarget
     button.disabled = true
     this.imageTarget.classList.add('disabled')
 
     fetch('https://picsum.photos/700/500').then((response) => {
       this.imageTarget.src = response.url
-
       this.imageTarget.classList.remove('disabled')
       button.disabled = false
     })
