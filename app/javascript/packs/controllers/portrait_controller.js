d89baed7227b55501eb88c2c89d1b9184191d6b7
miguejs <imiked.mail@gmail.com>
2020-03-10 18:29:53 -0600

    Finish adding actions and targets

---
+++ app/javascript/packs/controllers/portrait_controller.js
@@ -0,0 +1,25 @@
+import { Controller } from 'stimulus'
+
+export default class extends Controller {
+  static targets = ['image']
+  connect() {
+    console.log("Hello, Stimulus!", this.element)
+    console.log("Portrait image is ", this.imageTarget)
+  }
+
+  changePicture(event) {
+    console.log("Fired Event", event)
+    console.log("Element with the data action", event.currentTarget)
+    var button = event.currentTarget
+
+    button.disabled = true
+    this.imageTarget.classList.add('disabled')
+
+    fetch('https://picsum.photos/700/500').then((response) => {
+      this.imageTarget.src = response.url
+
+      this.imageTarget.classList.remove('disabled')
+      button.disabled = false
+    })
+  }
+}
