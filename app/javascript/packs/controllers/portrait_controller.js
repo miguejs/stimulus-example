a64a7d5758d9bb9335075dabb71e038dbb996485
miguejs <imiked.mail@gmail.com>
2020-03-12 15:53:12 -0600

    adding data maps

--- app/javascript/packs/controllers/portrait_controller.js
+++ app/javascript/packs/controllers/portrait_controller.js
@@ -1,11 +1,19 @@
 import { Controller } from 'stimulus'
-
+const axios = require('axios')
 export default class extends Controller {
-  static targets = ['image', 'changeButton']
+  static targets = ['image', 'changeButton', 'author']
+  connect() {
+    this.changePicture()
+  }
+
+  get url() {
+    return this.data.get('url')
+  }
 
   async changePicture(event) {
     this.toggleAvailability()
     this.imageTarget.src = await this.getRamdomPicture()
+    this.setImageDetails()
     this.toggleAvailability()
   }
 
@@ -14,8 +22,23 @@ export default class extends Controller {
     this.imageTarget.classList.toggle('disabled')
   }
 
-  async getRamdomPicture() {
-    const response = await fetch('https://picsum.photos/700/500')
+  async getRamdomPicture(width=700, height=500) {
+    const response = await fetch(`${this.url}/${width}/${height}`)
     return response.url
+  }
+
+  async setImageDetails() {
+    const { author } = await this.getImageDetails()
+    this.authorTarget.innerHTML = author
+  }
+
+  idFromImage() {
+    return this.imageTarget.src.split('/')[4]
+  }
+
+  async getImageDetails() {
+    const id = this.idFromImage()
+    const response = await axios.get(`${this.url}/id/${id}/info`)
+    return response.data
   }
 }
